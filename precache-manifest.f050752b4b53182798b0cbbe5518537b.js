5176252e137e5f60a428334b1d834dcf833fe936
Adarsh Raut <adarshraut0987@gmail.com>
2023-10-07 11:01:16 +0530

    Updates

---
+++ precache-manifest.f050752b4b53182798b0cbbe5518537b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e45dc15f281bfd22705150c606b17e",
+    "url": "/keeper-notes-app/index.html"
+  },
+  {
+    "revision": "04bb28a8766bceec9e2c",
+    "url": "/keeper-notes-app/static/js/2.376f357d.chunk.js"
+  },
+  {
+    "revision": "db74df88719290dfad99",
+    "url": "/keeper-notes-app/static/js/main.c25eac57.chunk.js"
+  },
+  {
+    "revision": "812aee702c249195265a",
+    "url": "/keeper-notes-app/static/js/runtime-main.c8e82aae.js"
+  }
+]);
